de4bba20f0db852c843c2d473e25390f500ebc4a
Amiel Martin <amiel@carnesmedia.com>
2014-06-06 09:27:18 -0700

    Use isBlank where possible

--- src/first-present.js
+++ src/first-present.js
@@ -1,15 +1,16 @@
-import {A, get, isBlank, computed} from 'ember';
+import {A, get, isBlank, isEmpty, computed} from 'ember';
 
 var a_slice = Array.prototype.slice;
 
 export default function EmberCPM_firstPresent() {
   var properties = a_slice.call(arguments);
   var computedArgs = a_slice.call(properties);
+  var checker = isBlank || isEmpty; // isBlank was introduced in Ember 1.5
 
   computedArgs.push(function() {
     var that = this;
     var property = A(properties).find(function(key) {
-      return ! isBlank(get(that, key));
+      return ! checker(get(that, key));
     });
 
     if (property) { return get(that, property); }
