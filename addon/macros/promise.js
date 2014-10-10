9163c6402110a617f80ec95c93848d2d5c8b3646
Miguel Camba <miguel.camba@gmail.com>
2014-10-10 05:11:55 +0100

    Move .jshintrc

--- addon/macros/promise.js
+++ addon/macros/promise.js
@@ -2,13 +2,12 @@ import Ember from 'ember';
 
 var get = Ember.get;
 var computed = Ember.computed;
-var $ = Ember.$;
 
 // TODO: Use RSVP?
 export default function EmberCPM_promise(dependentKey) {
   return computed(dependentKey, function(){
     var value = get(this, dependentKey);
     if (value == null) { return value; }
-    return $.when(value);
+    return Ember.$.when(value);
   });
 }
