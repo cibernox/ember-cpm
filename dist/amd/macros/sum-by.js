47cbb4052e412c1c0afd109eb6ee4fa9dccc1271
Miguel Camba <miguel.camba@gmail.com>
2014-10-16 00:52:29 +0100

    Update dist files

--- dist/amd/macros/sum-by.js
+++ dist/amd/macros/sum-by.js
@@ -7,24 +7,6 @@ define(
     var get = Ember.get;
     var reduceComputed = Ember.reduceComputed;
 
-    // function logWarning(typeName, propName) {
-    //   var message = '[DEPRECATED: EmberCPM/sumBy] - %@ - %@'.fmt(typeName, propName);
-    //   if (console.groupCollapsed) {
-    //     // modern browsers
-    //     console.groupCollapsed(message);
-    //     console.info('Please use a combination of EmberCPM.Macros.sum and EmberCPM.Macros.mapBy');
-    //     console.info('EmberCPM.Macros.sum(EmberCPM.Macros.mapBy("list", "value"))');
-    //     if (console.trace) {
-    //       console.trace();
-    //     }
-    //     console.groupEnd();
-    //   }
-    //   else {
-    //     // legacy browsers
-    //     Ember.Logger.warn(message);
-    //   }
-    // }
-
     /**
      * DEPRECATED - 10/14/2014
      * Rather than use sumBy, developers should use composed computed property macros
@@ -48,12 +30,10 @@ define(
         initialValue: 0.0,
 
         addedItem: function(accumulatedValue, item /*, changeMeta, instanceMeta */){
-          // logWarning(this.constructor.toString(), dependentKey);
           return accumulatedValue + parseFloat(get(item, propertyKey));
         },
 
         removedItem: function(accumulatedValue, item /*, changeMeta, instanceMeta */){
-          // logWarning(this.constructor.toString(), dependentKey);
           return accumulatedValue - parseFloat(get(item, propertyKey));
         }
       });
