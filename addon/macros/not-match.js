17d877b1d1e0ec92f3d6c078e67935782814eb99
Miguel Camba <miguel.camba@gmail.com>
2014-10-19 00:49:31 +0100

    Document `notEqual` and `notMatch`

--- addon/macros/not-match.js
+++ addon/macros/not-match.js
@@ -3,6 +3,14 @@ import Ember from 'ember';
 var get = Ember.get;
 var computed = Ember.computed;
 
+/**
+  Opposite or `Ember.computed.match`
+
+  @method macros.notMatch
+  @param {String} dependentKey Dependent key which value must not be match to the given regexp.
+  @param {String} regexp       Regular expression to compare with.
+  @return {Boolean} Returns true if the value in dependentKey does not match the given regexp.
+*/
 export default function EmberCPM_notMatch(dependentKey, regexp) {
   return computed(dependentKey, function(){
     var value = get(this, dependentKey);
