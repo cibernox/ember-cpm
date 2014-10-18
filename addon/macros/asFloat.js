84dd3187b2bbe6da516ec48632294e4a31893a7b
Miguel Camba <miguel.camba@gmail.com>
2014-10-18 01:40:52 +0100

    Add docs to `among`, `asFloat` and `asInt`

--- addon/macros/asFloat.js
+++ addon/macros/asFloat.js
@@ -1,3 +1,23 @@
 import {parseComputedPropertyMacro} from '../utils';
 
+/**
+  Converts the value in the given dependent key into a float.
+
+  Note that it returns NaN when the dependent key contains `null`, `undefined` or an invalid string.
+
+  Example
+
+  ```javascript
+  var item = Ember.Object.extend({
+    stringPrice: '123.45',
+    price: asFloat('stringPrice')
+  }).create();
+
+  item.get('price'); // 123.45
+  ```
+
+  @method macros.asFloat
+  @param {String} dependentKey Depedent key which value will be casted to a float.
+  @return {Number} Returns casted float.
+*/
 export default parseComputedPropertyMacro (parseFloat);
--- addon/macros/asInt.js
+++ addon/macros/asInt.js
@@ -1,5 +1,27 @@
 import {parseComputedPropertyMacro} from '../utils';
 
+/**
+  Converts the value in the given dependent key into an integer.
+
+  Note that it returns NaN when the dependent key contains `null`, `undefined` or an invalid string.
+
+  Example
+
+  ```javascript
+  var item = Ember.Object.extend({
+    stringPrice: '123',
+    price: asInt('stringPrice')
+    invalid: asInt('abc')
+  }).create();
+
+  item.get('price');   // 123
+  item.get('invalid'); // NaN
+  ```
+
+  @method macros.asInt
+  @param {String} dependentKey Depedent key which value will be casted to a integer.
+  @return {Number} Returns casted integer.
+*/
 export default parseComputedPropertyMacro(function (raw) {
   return parseInt(raw, 10);
 });
