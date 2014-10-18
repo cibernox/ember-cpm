775b4ad40232aea783b153b0062716cde054bc26
Miguel Camba <miguel.camba@gmail.com>
2014-10-18 01:22:35 +0100

    Add YUIDoc to allEqual

--- addon/macros/all-equal.js
+++ addon/macros/all-equal.js
@@ -1,7 +1,32 @@
-
 import Ember from 'ember';
 import {getVal, getDependentPropertyKeys} from '../utils';
 
+/**
+  Returns true if all the all its dependent values are equal between them.
+
+  Example
+
+  ```javascript
+  var Cuboid = Ember.Object.extend({
+    cube: allEqual('height', 'width', 'depth')
+  });
+
+  var shape = Cuboid.create({
+    height: 6,
+    width: 6,
+    depth: 6
+  });
+
+  shape.get('cube'); // true
+  shape.set('width', 4);
+  shape.get('cube'); // false
+  ```
+
+  @method macros.allEqual
+  @param *arguments Values or dependent keys that must be equal. It can be a values or the key of a
+                    property in the object of the computed property.
+  @return {Boolean} Returns true it all elements are equal
+*/
 export default function EmberCPM_allEqual() {
   var mainArguments = Array.prototype.slice.call(arguments); // all arguments
   var propertyArguments = getDependentPropertyKeys(mainArguments);
