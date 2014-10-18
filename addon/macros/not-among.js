3ad5353170aac192d9f44b9e49a303be3d8ef098
Mike North <northm@yahoo-inc.com>
2014-10-18 00:58:07 -0700

    Fixes #76 - deprecate notEqual, notAmong, notMatch

--- addon/macros/not-among.js
+++ addon/macros/not-among.js
@@ -3,7 +3,19 @@ import Ember from 'ember';
 var get = Ember.get;
 var computed = Ember.computed;
 
+/**
+ * DEPRECATED - 10/18/2014
+ * Rather than use notAmong, developers should use composed computed property macros
+ *
+ * Example:
+ *
+ * 	myProp: not(among('val', 'a', 'b', 'c'))
+ */
+
+
 export default function EmberCPM_notAmong(dependentKey) {
+  Ember.deprecate("[DEPRECATED: EmberCPM/notAmong] Please use a combination of EmberCPM.Macros.not and EmberCPM.Macros.among \nEmberCPM.Macros.not(EmberCPM.Macros.among('val', 'a', 'b'))");
+
   var properties = Array.prototype.slice.call(arguments, 1);
 
   return computed(dependentKey, function(){
--- addon/macros/not-match.js
+++ addon/macros/not-match.js
@@ -3,7 +3,18 @@ import Ember from 'ember';
 var get = Ember.get;
 var computed = Ember.computed;
 
+/**
+ * DEPRECATED - 10/18/2014
+ * Rather than use notMatch, developers should use composed computed property macros
+ *
+ * Example:
+ *
+ * 	myProp: not(Ember.computed.match('email', /^.+@.+\\..+$/))
+ */
+
 export default function EmberCPM_notMatch(dependentKey, regexp) {
+  Ember.deprecate("[DEPRECATED: EmberCPM/notMatch] Please use a combination of EmberCPM.Macros.not and Ember.computed.match \nEmberCPM.Macros.not(Ember.computed.match('email', /^.+@.+\\..+$/))");
+
   return computed(dependentKey, function(){
     var value = get(this, dependentKey);
 
