2cb92d9ba2d91e25b38ea5a2739f3e7c928b2600
Miguel Camba <miguel.camba@gmail.com>
2014-10-19 01:08:02 +0100

    Document `quotient`

--- addon/macros/quotient.js
+++ addon/macros/quotient.js
@@ -1,9 +1,29 @@
 import Ember from 'ember';
 import {getVal, getDependentPropertyKeys} from '../utils';
 
+/**
+  Returns an the float quotient of divide the first argument by the second one.
+
+  Example
+
+  ```javascript
+  var MyType = Ember.Object.extend({
+    a: 6,
+    b: 2,
+    c: 5,
+    d: quotient('a', 'b'),                    // 3
+    e: quotient('a', 3),                      // 2
+    f: quotient(Ember.computed.alias('c'), 2) // 2.5
+  });
+  ```
+
+  @method macros.quotient
+  @param dividend Can be a number, a property key containing a number or another computed property.
+  @return {Float} The quotient of the division.
+*/
 export default function EmberCPM_quotient() {
-  var mainArguments = Array.prototype.slice.call(arguments), // all arguments
-    propertyArguments = getDependentPropertyKeys(mainArguments);
+  var mainArguments = Array.prototype.slice.call(arguments);
+  var propertyArguments = getDependentPropertyKeys(mainArguments);
 
   propertyArguments.push(function () {
     switch (mainArguments.length) {
