3c2529f19b2e7fb96868195f6191224c5262b7c4
Jonathan Jackson <jonathan.jackson1@me.com>
2016-01-05 12:27:03 -0500

    Add computed promise macro
    Updates computed property when supplied callback (which must return a promise) is resolved.
    
    Example
    
    ```javascript
    const myObject = Ember.Object.extend({
      data: computedPromise('dep', function(){
        return ajax('/data.json');
      })
    });
    
    myObject.get('data') // => undefined
    
    // once resolved
    
    myObject.get('data') // => { foo: 'bar' };
    ```
    
    For example, if you have a template that renders `data` when the promise is resolved the template will be updated.
    
    /ht @rwjblue (for help / pairing time) :beers:

---
+++ tests/unit/macros/computed-promise-test.js
@@ -0,0 +1,93 @@
+import { module, test } from "qunit";
+import Ember from "ember";
+import computedPromise from "ember-cpm/macros/computed-promise";
+
+var object;
+var deferred;
+
+module("promise-object", {
+  setup: function(){
+    deferred = Ember.RSVP.defer();
+
+    object = Ember.Object.extend({
+      dep: null,
+      promise: deferred.promise,
+      myComputedPromise: computedPromise('dep', function(){
+        return this.get('promise');
+      })
+    }).create();
+
+  }
+});
+
+test('updates when resolved', function(assert){
+  const done  = assert.async();
+  assert.expect(1);
+
+  // Must kick off promise before we can assert against proper value on next
+  // tick
+  object.get('myComputedPromise');
+
+  deferred.promise.then(function() {
+    assert.equal(object.get('myComputedPromise').foo, 'bar');
+
+    done();
+  });
+
+  deferred.resolve({ foo: 'bar' });
+});
+
+test('when dependent key changes cb is invoked (non-eagerly)', function(assert){
+  const done  = assert.async();
+  assert.expect(2);
+
+  // Must kick off promise before we can assert against proper value on next
+  // tick
+  object.get('myComputedPromise');
+
+  deferred.promise.then(function() {
+    assert.equal(object.get('myComputedPromise').foo, 'bar');
+  }).then(function(){
+    object.set('dep', 'does not matter');
+  }).then(function(){
+    deferred = Ember.RSVP.defer();
+
+    object.set('promise', deferred.promise);
+    object.get('myComputedPromise');
+
+    deferred.resolve({ foo: 'baz' });
+
+    return deferred.promise;
+  }).then(function(){
+    assert.equal(object.get('myComputedPromise').foo, 'baz');
+    done();
+  });
+
+  // kick it all off
+  deferred.resolve({ foo: 'bar' });
+});
+
+test('if fn not last argument throws', function(assert){
+    assert.throws(function(){
+      computedPromise('dep');
+    }, /You must supply a function as the last argument to this macro./);
+});
+
+test('works when not given a cb who returns a promise', function(assert){
+  const done  = assert.async();
+  assert.expect(1);
+
+  object.set('promise', { foo: 'bar' });
+
+  // Must kick off promise before we can assert against proper value on next
+  // tick
+  object.get('myComputedPromise');
+
+  deferred.promise.then(function() {
+    assert.equal(object.get('myComputedPromise').foo, 'bar');
+
+    done();
+  });
+
+  deferred.resolve();
+});
